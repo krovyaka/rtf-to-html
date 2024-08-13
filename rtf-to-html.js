61106e8442711a656338ba58cac8677e6883ede8
krovyaka <krovyaka1@yandex.ru>
2024-08-13 11:29:02 +0300

    fix

--- rtf-to-html.js
+++ rtf-to-html.js
@@ -1,62 +1,62 @@
 'use strict'
 
-const RTFSpan = require('./rtf-span');
-const RTFParagraph = require('./rtf-paragraph');
-const RTFDocument = require('./rtf-document');
+const RTFSpan = require('./rtf-span')
+const RTFParagraph = require('./rtf-paragraph')
+const RTFDocument = require('./rtf-document')
 
 class RtfToHtml {
-    toHtmlParagraph = (rftParagraph) => {
-        let result = '<p>';
-    
-        if (rftParagraph?.content) {
-            for (const content of rftParagraph.content) {
-                result += toHtmlElement(content);
-            }
-        }
-        result += '</p>'
-        return result;
+  toHtmlParagraph (rftParagraph) {
+    let result = '<p>'
+
+    if (rftParagraph && rftParagraph.content) {
+      for (const content of rftParagraph.content) {
+        result += this.toHtmlElement(content)
+      }
     }
-    
-    toHtmlSpan = (rftSpan) => {
-        const value = rftSpan && rftSpan.value && rftSpan.value.replace(/<[^>]*>/g, '');
-        if (value) {
-            let htmlStyle = '';
-            const font = rftSpan.style?.font;
-            if (font) {
-                htmlStyle += 'font-family: "' + font.name + '"'
-                if (font.family) {
-                    htmlStyle += ', ' + font.family;
-                }
-                htmlStyle += '; ';
-            }
-            if (rftSpan.style?.fontSize) {
-                htmlStyle += `font-size: ${ rftSpan.style.fontSize }px; `;
-            }
-            return `<span${ htmlStyle && ` style="${ htmlStyle }"` }>${ value }</span>`;
+    result += '</p>'
+    return result
+  }
+
+  toHtmlSpan (rftSpan) {
+    const value = rftSpan && rftSpan.value && rftSpan.value.replace(/<[^>]*>/g, '')
+    if (value) {
+      let htmlStyle = ''
+      const font = rftSpan.style && rftSpan.style.font
+      if (font) {
+        htmlStyle += 'font-family: "' + font.name + '"'
+        if (font.family) {
+          htmlStyle += ', ' + font.family
         }
-        return '';
+        htmlStyle += '; '
+      }
+      if (rftSpan.style && rftSpan.style.fontSize) {
+        htmlStyle += `font-size: ${rftSpan.style.fontSize}px; `
+      }
+      return `<span${htmlStyle && ` style="${htmlStyle}"`}>${value}</span>`
     }
-    
-    toHtmlDocument = (rftDocument) => {
-        let result = '';
-        if (rftDocument?.content) {
-            for (const content of rftDocument.content) {
-                result += toHtmlElement(content);
-            }
-        }
-        return result;
+    return ''
+  }
+
+  toHtmlDocument (rftDocument) {
+    let result = ''
+    if (rftDocument && rftDocument.content) {
+      for (const content of rftDocument.content) {
+        result += this.toHtmlElement(content)
+      }
     }
-    
-    toHtmlElement = (rftElement) => {
-        if (rftElement instanceof RTFParagraph) {
-            return toHtmlParagraph(rftElement);
-        } else if (rftElement instanceof RTFSpan) {
-            return toHtmlSpan(rftElement);
-        } else if (rftElement instanceof RTFDocument) {
-            return toHtmlDocument(rftElement);
-        }
-        return '';
+    return result
+  }
+
+  toHtmlElement (rftElement) {
+    if (rftElement instanceof RTFParagraph) {
+      return this.toHtmlParagraph(rftElement)
+    } else if (rftElement instanceof RTFSpan) {
+      return this.toHtmlSpan(rftElement)
+    } else if (rftElement instanceof RTFDocument) {
+      return this.toHtmlDocument(rftElement)
     }
+    return ''
+  }
 }
 
 // parseRTF.string(rtfContent, (err, doc) => {
